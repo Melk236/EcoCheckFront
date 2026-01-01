14fa9b479d0e5c3b153f76607cfcbc06f2e2a9ff
Melk236 <162185560+Melk236@users.noreply.github.com>
2026-01-01 14:44:19 +0100

    Creacion del componente de la lista de empresas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,13 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { DetalleProductoComponent } from './components/home/detalle-producto/detalle-producto.component';
+import { ListaEmpresas } from './components/lista-empresas/lista-empresas.component';
 
 export const routes: Routes = [
 
     {path:'',redirectTo:'home',pathMatch:'full'},
     {path:'home', component:HomeComponent},
-    {path:'home/detalle-producto/:id',component:DetalleProductoComponent}
+    {path:'home/detalle-producto/:id',component:DetalleProductoComponent},
+    {path:'empresas', component:ListaEmpresas}
     
 ];
---
+++ src/app/components/lista-empresas/lista-empresas.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-lista-empresas',
+  imports: [],
+  templateUrl: './lista-empresas.html',
+  styleUrl: './lista-empresas.css',
+})
+export class ListaEmpresas {
+
+}
