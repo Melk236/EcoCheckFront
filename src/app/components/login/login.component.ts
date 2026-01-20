90fe16d9f30b9341b5f6d9a47c27cc39bdc4251e
Melk236 <162185560+Melk236@users.noreply.github.com>
2026-01-20 17:06:24 +0100

    Lógica del login

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,11 +1,54 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnDestroy } from '@angular/core';
+import { FormBuilder, FormControl,  FormGroup,  ReactiveFormsModule, Validators } from '@angular/forms';
+import { Subject, takeUntil } from 'rxjs';
+import { AuthUser } from '../../types/auth-user';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [ReactiveFormsModule,CommonModule],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
-export class LoginComponent {
+export class LoginComponent implements OnDestroy {
 
+  //Variable para manejar el formulario
+  formulario:FormGroup;
+
+  //Observable para la desuscripción de los observables
+  destroy$=new Subject<void>();
+
+  /*Inicialización del formcontrol */
+  constructor(private fb:FormBuilder,private authService:AuthService){
+
+    this.formulario=this.fb.group({
+      user:new FormControl('',[Validators.required,Validators.minLength(6),Validators.maxLength(20),Validators.pattern(/^[a-zA-Z0-9_]*$/)]),
+      password:new FormControl('',new FormControl('',[Validators.required,Validators.minLength(6),Validators.maxLength(20),Validators.pattern(/^[a-zA-Z0-9_]*$/)]))
+    });
+  }
+  
+
+  /*Método para enviar los datos del usuario al servidor */
+  onSubmit(){
+    const body:AuthUser={
+      user: this.formulario.get('user')?.value,
+      password: this.formulario.get('password')?.value
+    };
+
+    this.authService.login(body).pipe(takeUntil(this.destroy$)).
+    subscribe({
+      next:(data)=>{
+        console.log(data);
+      },
+      error:(error)=>{
+        console.log(error);
+      }
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,17 +1,19 @@
 import { Injectable } from '@angular/core';
 import { environment } from '../environment/environment';
 import { HttpClient } from '@angular/common/http';
+import { AuthUser } from '../types/auth-user';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
-  private readonly url=environment.apiUrl;
+  private readonly url=environment.apiUrl+'Auth/login';
 
   constructor(private http:HttpClient){}
 
-  login(){
-
+  login(usuario:AuthUser):Observable<{token:string}>{
+    return this.http.post<{token:string}>(this.url,usuario);
   }
 
   register(){}
