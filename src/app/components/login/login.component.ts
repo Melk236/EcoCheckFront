53ff3ca6bb76020f957ba71ae7c437e14a4111c2
Melk236 <162185560+Melk236@users.noreply.github.com>
2026-01-24 17:51:57 +0100

    Fix bugs en home

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,95 +1,95 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { FormBuilder, FormControl,  FormGroup,  ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Subject, takeUntil } from 'rxjs';
 import { AuthUser } from '../../types/auth-user';
 import { AuthService } from '../../services/auth.service';
-import { Router } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 import { AlertaComponent } from "../modales/alerta/alerta.component";
 import { ProfileService } from '../../services/profile.service';
 
 @Component({
   selector: 'app-login',
-  imports: [ReactiveFormsModule, CommonModule, AlertaComponent],
+  imports: [ReactiveFormsModule, CommonModule, AlertaComponent, RouterLink],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
-export class LoginComponent implements OnInit,OnDestroy {
+export class LoginComponent implements OnInit, OnDestroy {
 
   //Variable para manejar el formulario
-  formulario:FormGroup;
+  formulario: FormGroup;
 
   //Variable para el mensaje del error
-  mensaje:string='';
+  mensaje: string = '';
 
   //Variable manejo del spinner
-  cargando:boolean=false;
+  cargando: boolean = false;
   //Observable para la desuscripción de los observables
-  destroy$=new Subject<void>();
+  destroy$ = new Subject<void>();
 
   /*Inicialización del formGroup */
-  constructor(private fb:FormBuilder,private authService:AuthService,private route:Router,private profileService:ProfileService){
+  constructor(private fb: FormBuilder, private authService: AuthService, private route: Router, private profileService: ProfileService) {
 
-    this.formulario=this.fb.group({
-      user:new FormControl('',Validators.required),
-      password:new FormControl('',Validators.required)
+    this.formulario = this.fb.group({
+      user: new FormControl('', Validators.required),
+      password: new FormControl('', Validators.required)
     });
   }
   ngOnInit(): void {
-   this.getUser();
+    this.getUser();
   }
-  
+
 
   /*Método para enviar los datos del usuario al servidor */
-  onSubmit(){
+  onSubmit() {
     //Activamos el spinner
-    this.cargando=true;
+    this.cargando = true;
 
-    const body:AuthUser={
+    const body: AuthUser = {
       user: this.formulario.get('user')?.value,
       password: this.formulario.get('password')?.value
     };
 
     this.authService.login(body).pipe(takeUntil(this.destroy$)).
-    subscribe({
-      next:(data)=>{
-        this.cargando=false;//esactivamos el spinner
-        this.saveToken(data.token)//Guardamos el token en el 
-        this.route.navigate(['home']);//Navegamos al componente Home
-      },
-      error:(error)=>{
-        this.cargando=false;
-        this.mensaje=error.error.mensaje;
-        this.closeAlert();
-      }
-    });
+      subscribe({
+        next: (data) => {
+          this.cargando = false;//esactivamos el spinner
+          this.saveToken(data.token)//Guardamos el token en el 
+          this.route.navigate(['home']);//Navegamos al componente Home
+        },
+        error: (error) => {
+          this.cargando = false;
+          this.mensaje = error.error.mensaje;
+          this.closeAlert();
+        }
+      });
   }
 
   /*Guardamos el token en el sessionSotrage */
-  saveToken(token:string){
+  saveToken(token: string) {
 
-    sessionStorage.setItem('jwt',token);
+    sessionStorage.setItem('jwt', token);
 
   }
 
   /*Añadimos un setTimeout para que la alerta desaparezca 
   en un tiempo determinado*/
-  closeAlert(){
+  closeAlert() {
     setTimeout(() => {
-      this.mensaje='';  
+      this.mensaje = '';
     }, 2500);
   }
 
-  getUser(){
+  getUser() {
     this.profileService.getUser().pipe(takeUntil(this.destroy$)).
-    subscribe({
-      next:(data)=>{
-        console.log(data);
-      },
-      error:(error)=>{
-        console.log(error);
-      }
-    })
+      subscribe({
+        next: (data) => {
+          console.log(data);
+        },
+        error: (error) => {
+          console.log(error);
+        }
+      })
   }
 
   ngOnDestroy(): void {
--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-sidebar',
-  imports: [],
+  imports: [RouterLink, CommonModule],
   templateUrl: './sidebar.html',
   styleUrl: './sidebar.css',
 })
 export class Sidebar {
+  dropdownOpen = false;
+  constructor(private router: Router) { }
 
+  isActive(route: string): boolean {
+    return this.router.url.includes(route);
+  }
 }
